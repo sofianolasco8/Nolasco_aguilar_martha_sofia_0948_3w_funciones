a32d139336099b1c982e49fc2e9ce6aca206dc57
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 20:11:11 -0600

    Add files via upload

---
+++ practicando_funciones_4.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(nombre, apellido):#se le da valor a la funcion
+  print(nombre  + " " + apellido)#indica que deben de mostrarse juntos 
+
+my_function("sofia", "nolasco")#indica cuales son los valors que debe dar la funcion 
