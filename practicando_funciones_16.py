936062ca711c64aac81d5e0bc860c3defbc13824
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 23:20:00 -0600

    Add files via upload

---
+++ practicando_funciones_16.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(*, x):#se abre la funcion que solo puede tener palabras clave
+  print(x)
+
+my_function(x = 6)#se imprime el valor de la funcion
