edd712873da56348b0fd1d4d3cf76c8d513bb4e5
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 20:24:12 -0600

    Add files via upload

---
+++ practicando_funciones_6.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3W")
+print(" ")
+def my_function(*edades):#se abre la funcion para indicar quien es menor 
+    print("la persona menor es " + edades[2])#da el mensaje de que la persona menor es el numero dos
+
+my_function("bri", "sof", "dayana")#los valores de la funcion
