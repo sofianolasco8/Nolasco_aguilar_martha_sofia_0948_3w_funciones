189cabbc5813d4a1213aa82d3cf0208559073a58
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 23:03:36 -0600

    Add files via upload

---
+++ practicando_funciones_13.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(x, /):#se abre la funcion
+  print(x) #imprime si x esta dentro de la funcion 
+
+my_function(7)#le da valor a la funcion
