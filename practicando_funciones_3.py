351385788046fe43aa9d2206afec45321b9b3d4a
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 20:04:28 -0600

    Add files via upload

---
+++ practicando_funciones_3.py
@@ -0,0 +1,9 @@
+print(" ")
+print("Nolasco_Aguilar_Martha_Sofia_0948_3W")
+print(" ")
+def my_function(fname): #se le da valor a la funcion 
+    print(fname + "niña")#indica que debe de imprimir la funcion mas la palabra niña 
+
+my_function("bri")#indica cual es el valor de la funcion 
+my_function("dayana")#indica cual es el valor de la funcion 
+my_function("sof")#indica cual es el valor de la funcion 
