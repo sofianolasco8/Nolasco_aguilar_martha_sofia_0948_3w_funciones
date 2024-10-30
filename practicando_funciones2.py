e97b30e892b3ad0ba1f62d52eee255c18f62db6d
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 19:53:59 -0600

    Add files via upload

---
+++ practicando_funciones2.py
@@ -0,0 +1,6 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3W")
+print(" ")
+def my_function():#se define la funcion
+    print("Hello from a function")#indica lo que debe decir la funcion
+my_function()#imprime la funcion 
