93e47ac56d2671f372d6f64a65bade7d467b6482
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 22:34:19 -0600

    Add files via upload

---
+++ practicando_funciones_10.py
@@ -0,0 +1,10 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(ropa):
+  for x in ropa:
+    print(x)
+
+ropa = ["blusa", "pantalon", "calcetines"]
+
+my_function(ropa)
