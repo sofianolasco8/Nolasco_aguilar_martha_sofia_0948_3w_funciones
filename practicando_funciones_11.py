ae138d00ecbdfad5c6349b71d05d4946c431714a
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 22:46:38 -0600

    Add files via upload

---
+++ practicando_funciones_11.py
@@ -0,0 +1,9 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(x):#abre la funcion 
+  return 6 * x #permite que una funcion vuelva a su valor
+
+print(my_function(6))#se imprime la funcion
+print(my_function(2))#se imprime la funcion
+print(my_function(10))#se imprime la funcion
