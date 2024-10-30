0816986309d36af7dcf154e094513afc295e1a7f
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 23:31:26 -0600

    Add files via upload

---
+++ praticando_funciones_18.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(*, x):#se abre la funcion
+  print(x)
+
+my_function(2)#en esta parte se mostrara error porque se intenta enviar un argumento posicional
