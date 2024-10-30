90f9dfdf9bb658710264f10e419d7e21df34214e
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 20:36:43 -0600

    Add files via upload

---
+++ practicando_funciones_8.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3W")
+print(" ")
+def my_function(**persona):#se abre la funcion 
+  print("mi nombre es  " + persona ["nombre"])#se indica lo que se debe de agregar a la funcion 
+
+my_function(nombre = "sofia", apellido = "nolasco")#se le agregan valores a la funcion
