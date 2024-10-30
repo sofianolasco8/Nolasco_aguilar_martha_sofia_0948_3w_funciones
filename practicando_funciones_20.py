84109ca51c58526a04f7601aa78959755328d70c
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 23:45:30 -0600

    Add files via upload

---
+++ practicando_funciones_20.py
@@ -0,0 +1,13 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3W")
+print(" ")
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
