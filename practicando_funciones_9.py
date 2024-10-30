475bf8a458913c8ab01cbe9f4c15afcde0b9d7a6
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 22:25:55 -0600

    Add files via upload

---
+++ practicando_funciones_9.py
@@ -0,0 +1,10 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3W")
+print(" ")
+def my_function(colores = "todos"):
+  print("mi color favoito es " + colores)
+
+my_function("rojo")
+my_function("amarillo")
+my_function()
+my_function("azul")
