1a3f65ef83b80ff92c3bc95480eafe26ef862c1d
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 20:16:28 -0600

    Add files via upload

---
+++ practicando_funciones_5.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(nombre, apellido):#le da valor a la funcion 
+  print(nombre + " " + apellido)#indica lo que se debe de marcar en la pantalla
+
+my_function("sof")#en esta parte marcaria error al correrlo porque se esperan dos valores 
