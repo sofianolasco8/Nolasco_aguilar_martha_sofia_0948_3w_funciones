1695500f762aaf43ac511771d71e507dfbfcf3b5
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-29 20:31:35 -0600

    Add files via upload

---
+++ practicando_funciones_7.py
@@ -0,0 +1,7 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+def my_function(persona3, persona2, persona1):#abre la funcion 
+  print("la persona menor es  " + persona3)#indica el mensaje que debe mostrar en pantalla
+
+my_function(persona1 = "bri", persona2 = "sof", persona3 = "dayana")#le da valor a la funcion 
